2f7b0179828fc93e170fb16e59e4821e29a0cd82
Rafael OLiveira <37752682+blasther12@users.noreply.github.com>
2024-06-05 21:41:07 -0300

    chore: add endpoints folder

---
+++ src/Saphira.Api/Endpoints/WeatherforecastEndpoints.cs
@@ -0,0 +1,36 @@
+namespace Saphira.Api.Endpoints
+{
+    public static class WeatherforecastEndpoints
+    {
+        public static void MapWeatherforecastEndpoints(this IEndpointRouteBuilder builder)
+        {
+            var group = builder.MapGroup("api/weatherforecast")
+                .WithTags("Weatherforecast");
+
+            var summaries = new[]
+            {
+                "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            };
+
+            group.MapGet("/", () =>
+            {
+                var forecast = Enumerable.Range(1, 5).Select(index =>
+                    new WeatherForecast
+                    (
+                        DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                        Random.Shared.Next(-20, 55),
+                        summaries[Random.Shared.Next(summaries.Length)]
+                    ))
+                    .ToArray();
+                return forecast;
+            })
+            .WithName("GetWeatherForecast")
+            .WithOpenApi();
+        }
+
+        record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+        {
+            public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+        }
+    }
+}
