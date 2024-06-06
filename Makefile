30f5b2af390f99b7eb1c392df6e8f94819b323a2
Rafael OLiveira <37752682+blasther12@users.noreply.github.com>
2024-06-05 21:21:53 -0300

    ci: add action
    * ci: add action
    
    * ci: add action
    
    * ci: add action
    
    * ci: add action

--- Makefile
+++ Makefile
@@ -1,2 +1,8 @@
 dev:
-	dotnet run --project src\Saphira.Api\Saphira.Api.csproj --watch
+	dotnet watch --project src\Saphira.Api\Saphira.Api.csproj
+
+start:
+	dotnet run --project src\Saphira.Api\Saphira.Api.csproj
+
+test:
+	dotnet test src\Saphira.Tests\Saphira.Tests.csproj /p:CollectCoverage=true
--- src/Saphira.Api/Program.cs
+++ src/Saphira.Api/Program.cs
@@ -3,6 +3,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddHealthChecks();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
@@ -35,6 +36,8 @@ app.MapGet("/weatherforecast", () =>
 })
 .WithName("GetWeatherForecast")
 .WithOpenApi();
+
+app.MapHealthChecks("/api/health");
 
 app.Run();
 
